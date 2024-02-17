f414c47368dd304e9b4a6786ab8765f8cb946629
Jeff Mofor Allo <jeffmoforallo@Jeffs-MacBook-Air.local>
2024-02-17 13:54:30 -0500

    Created an input component and added last name and middle name fields

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { useState, useEffect } from "react";
 
+const InputField = ({ label, value, setValue, required }: { label: string, value: string, setValue: any, required: boolean}) => {
+	return (
+		<div className="flex items-center">
+			<h4>{label}</h4>
+			<input
+				type="text"
+				className="m-2 text-black"
+				value={value}
+				placeholder={label}
+				required={required}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</div>
+	);
+};
+
 export default function SignUp() {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
 	const [middleName, setMiddleName] = useState("");
+
 
 	return (
 		<main className="flex min-h-11/12 items-center justify-center">
@@ -16,17 +33,9 @@ export default function SignUp() {
 				<p className="text-sm">* marks a required field</p>
 				<p>{lastName}</p>
 				<p>{middleName}</p>
-				<div className="flex items-center">
-					<h4>First Name *</h4>
-					<input
-						type="text"
-						className="m-2 text-black"
-						value={firstName}
-						placeholder="FirstName *"
-						required={true}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-				</div>
+				<InputField label='First Name *' value = {firstName} setValue = {setFirstName} required={true}/>
+				<InputField label='Last Name *' value = {lastName} setValue = {setLastName} required={true}/>
+				<InputField label='Middle Name' value = {middleName} setValue = {setMiddleName} required={false}/>
 			</div>
 		</main>
 	);
