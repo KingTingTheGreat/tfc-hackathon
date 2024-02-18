7152049260fadb3c659abd577e5954949c175e16
KingTingTheGreat <jting@bu.edu>
2024-02-17 20:53:57 -0500

    properly returns error

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: NextRequest) {
 		!user.groupNumber ||
 		!user.rxBin
 	) {
-		return NextResponse.json({ status: 500 });
+		return NextResponse.json({ error: "Invalid input data" }, { status: 500 });
 	}
 
 	const db = await userDBConnect();
