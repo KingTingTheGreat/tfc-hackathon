039e8256b413373eb275b1852a7caf917d330fc0
ridanaeeem <ridan@bu.edu>
2024-02-18 11:13:07 -0500

    background good on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Link from "next/link";
+import Pills from "@/components/pills";
 
 const ButtonLink = ({ path, text }: { path: string; text: string }) => {
 	return (
 		<Link
-			className="text-blue text-3xl bg-white text-center py-2 px-4 my-2 rounded-full hover:text-blaj"
+			className="z-10 text-blue text-3xl bg-white text-center py-2 px-4 my-2 rounded-full hover:text-blaj"
 			href={path}>
 			{text}
 		</Link>
@@ -16,16 +17,18 @@ export default function Home() {
 
 	return (
 		<main className="flex flex-col min-h-11/12 items-center">
-			<h1 className="text-[20vw] md:text-[15vh] text-white text-4xl font-bold pt-[10vh] md:pt-[20vh] m-4">
+			<Pills />
+			<h1 className="z-10 text-[20vw] md:text-[15vh] text-white text-4xl font-bold pt-[10vh] md:pt-[30vh] m-4">
 				Explo<span className="text-black">Rx</span>
 			</h1>
-			<div className="text-center p-[5rem] text-white text-3xl">
+			<div className="z-10 text-center p-[5rem] text-white text-3xl">
 				Your one stop shop for finding and comparing prescription and medical procedure pricing information
 			</div>
 			<div className="flex flex-col justify-around">
 				<ButtonLink path="sign-up" text="Sign Up" />
 				<ButtonLink path="sign-in" text="Sign In" />
 			</div>
+			<div></div>
 		</main>
 	);
 }
---
+++ components/pills.tsx
@@ -0,0 +1,519 @@
+export default function Pills() {
+	return (
+		<div className="opacity-60 fixed w-full">
+			<svg width="100%" height="100%" viewBox="0 0 430 235" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<mask
+					id="mask0_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="303"
+					y="107"
+					width="53"
+					height="32">
+					<rect
+						x="357.91"
+						y="127.699"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(164.637 357.91 127.699)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask0_0_1)">
+					<rect
+						x="357.91"
+						y="127.699"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(164.637 357.91 127.699)"
+						fill="url(#paint0_linear_0_1)"
+					/>
+					<ellipse
+						cx="317.333"
+						cy="125.881"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(164.637 317.333 125.881)"
+						fill="white"
+					/>
+					<ellipse
+						cx="317.333"
+						cy="125.881"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(164.637 317.333 125.881)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask1_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="188"
+					y="67"
+					width="54"
+					height="31">
+					<rect
+						x="238.253"
+						y="100.028"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-168.154 238.253 100.028)"
+						fill="white"
+					/>
+				</mask>
+				{/* <g mask="url(#mask1_0_1)">
+					<rect
+						x="238.253"
+						y="100.028"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-168.154 238.253 100.028)"
+						fill="url(#paint1_linear_0_1)"
+					/>
+					<ellipse
+						cx="202.998"
+						cy="79.8575"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-168.154 202.998 79.8575)"
+						fill="white"
+					/>
+					<ellipse
+						cx="202.998"
+						cy="79.8575"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-168.154 202.998 79.8575)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask2_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="399"
+					y="168"
+					width="49"
+					height="42">
+					<rect
+						x="394.841"
+						y="194.237"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-34.8034 394.841 194.237)"
+						fill="white"
+					/>
+				</mask> */}
+				<g mask="url(#mask2_0_1)">
+					<rect
+						x="394.841"
+						y="194.237"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-34.8034 394.841 194.237)"
+						fill="url(#paint2_linear_0_1)"
+					/>
+					<ellipse
+						cx="433.71"
+						cy="182.446"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-34.8034 433.71 182.446)"
+						fill="white"
+					/>
+					<ellipse
+						cx="433.71"
+						cy="182.446"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-34.8034 433.71 182.446)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask3_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="376"
+					y="36"
+					width="50"
+					height="41">
+					<rect
+						x="416.665"
+						y="80.7357"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-146.815 416.665 80.7357)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask3_0_1)">
+					<rect
+						x="416.665"
+						y="80.7357"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-146.815 416.665 80.7357)"
+						fill="url(#paint3_linear_0_1)"
+					/>
+					<ellipse
+						cx="391.166"
+						cy="49.1199"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-146.815 391.166 49.1199)"
+						fill="white"
+					/>
+					<ellipse
+						cx="391.166"
+						cy="49.1199"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-146.815 391.166 49.1199)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask4_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="288"
+					y="-13"
+					width="46"
+					height="46">
+					<rect
+						x="283.902"
+						y="19.9697"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-44.6535 283.902 19.9697)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask4_0_1)">
+					<rect
+						x="283.902"
+						y="19.9697"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-44.6535 283.902 19.9697)"
+						fill="url(#paint4_linear_0_1)"
+					/>
+					<ellipse
+						cx="320.18"
+						cy="1.70332"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-44.6535 320.18 1.70332)"
+						fill="white"
+					/>
+					<ellipse
+						cx="320.18"
+						cy="1.70332"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-44.6535 320.18 1.70332)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask5_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="86"
+					y="95"
+					width="42"
+					height="49">
+					<rect
+						x="102.276"
+						y="90.3924"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(55.8996 102.276 90.3924)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask5_0_1)">
+					<rect
+						x="102.276"
+						y="90.3924"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(55.8996 102.276 90.3924)"
+						fill="url(#paint5_linear_0_1)"
+					/>
+					<ellipse
+						cx="113.589"
+						cy="129.402"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(55.8996 113.589 129.402)"
+						fill="white"
+					/>
+					<ellipse
+						cx="113.589"
+						cy="129.402"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(55.8996 113.589 129.402)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask6_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="115"
+					y="-13"
+					width="49"
+					height="43">
+					<rect
+						x="153.425"
+						y="34.0445"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-143.541 153.425 34.0445)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask6_0_1)">
+					<rect
+						x="153.425"
+						y="34.0445"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-143.541 153.425 34.0445)"
+						fill="url(#paint6_linear_0_1)"
+					/>
+					<ellipse
+						cx="129.773"
+						cy="1.02373"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-143.541 129.773 1.02373)"
+						fill="white"
+					/>
+					<ellipse
+						cx="129.773"
+						cy="1.02373"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-143.541 129.773 1.02373)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask7_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="4"
+					y="46"
+					width="33"
+					height="53">
+					<rect
+						x="38.9286"
+						y="49.8374"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(104.448 38.9286 49.8374)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask7_0_1)">
+					<rect
+						x="38.9286"
+						y="49.8374"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(104.448 38.9286 49.8374)"
+						fill="url(#paint7_linear_0_1)"
+					/>
+					<ellipse
+						cx="17.1794"
+						cy="84.1412"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(104.448 17.1794 84.1412)"
+						fill="white"
+					/>
+					<ellipse
+						cx="17.1794"
+						cy="84.1412"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(104.448 17.1794 84.1412)"
+						fill="white"
+					/>
+				</g>
+				<mask
+					id="mask8_0_1"
+					// style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="-20"
+					y="153"
+					width="51"
+					height="38">
+					<rect
+						x="24.0299"
+						y="195.084"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-153.391 24.0299 195.084)"
+						fill="white"
+					/>
+				</mask>
+				<g mask="url(#mask8_0_1)">
+					<rect
+						x="24.0299"
+						y="195.084"
+						width="53.0424"
+						height="24.248"
+						rx="12.124"
+						transform="rotate(-153.391 24.0299 195.084)"
+						fill="url(#paint8_linear_0_1)"
+					/>
+					<ellipse
+						cx="-4.92201"
+						cy="166.596"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-153.391 -4.92201 166.596)"
+						fill="white"
+					/>
+					<ellipse
+						cx="-4.92201"
+						cy="166.596"
+						rx="11.3662"
+						ry="16.2916"
+						transform="rotate(-153.391 -4.92201 166.596)"
+						fill="white"
+					/>
+				</g>
+				<defs>
+					<linearGradient
+						id="paint0_linear_0_1"
+						x1="357.91"
+						y1="139.823"
+						x2="389.357"
+						y2="139.823"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#93C3E8" />
+						<stop offset="0.484608" stop-color="#7BA6F0" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint1_linear_0_1"
+						x1="238.253"
+						y1="112.152"
+						x2="269.7"
+						y2="112.152"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#93C3E8" />
+						<stop offset="0.484608" stop-color="#7BA6F0" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint2_linear_0_1"
+						x1="394.841"
+						y1="206.361"
+						x2="426.288"
+						y2="206.361"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#93C3E8" />
+						<stop offset="0.484608" stop-color="#7BA6F0" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint3_linear_0_1"
+						x1="416.665"
+						y1="92.8597"
+						x2="448.111"
+						y2="92.8597"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#C1CDFE" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint4_linear_0_1"
+						x1="283.902"
+						y1="32.0937"
+						x2="315.349"
+						y2="32.0937"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#C1CDFE" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint5_linear_0_1"
+						x1="102.276"
+						y1="102.516"
+						x2="133.723"
+						y2="102.516"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#93C3E8" />
+						<stop offset="0.484608" stop-color="#7BA6F0" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint6_linear_0_1"
+						x1="153.425"
+						y1="46.1685"
+						x2="184.871"
+						y2="46.1685"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#93C3E8" />
+						<stop offset="0.484608" stop-color="#7BA6F0" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint7_linear_0_1"
+						x1="38.9286"
+						y1="61.9614"
+						x2="70.3752"
+						y2="61.9614"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#C1CDFE" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+					<linearGradient
+						id="paint8_linear_0_1"
+						x1="24.0299"
+						y1="207.208"
+						x2="55.4765"
+						y2="207.208"
+						gradientUnits="userSpaceOnUse">
+						<stop stop-color="#C1CDFE" />
+						<stop offset="0.9999" stop-color="#405FFF" stop-opacity="0.44" />
+						<stop offset="1" stop-color="white" />
+					</linearGradient>
+				</defs>
+			</svg>
+		</div>
+	);
+}
